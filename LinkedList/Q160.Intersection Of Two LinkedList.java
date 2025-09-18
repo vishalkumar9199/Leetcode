46a5b28b14acef6c704b1dbce32b9b24017d61a6
vishal <128027462+vishalkumar9199@users.noreply.github.com>
2025-09-19 01:41:10 +0530

    Create Q160.Intersection Of Two LinkedList.java

---
+++ LinkedList/Q160.Intersection Of Two LinkedList.java
@@ -0,0 +1,32 @@
+public class Solution {
+     private int getListLength(ListNode head) {
+    int len = 0;
+
+    while (head != null) {
+      len++;
+      head = head.next;
+    }
+
+    return len;
+  }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA=getListLength(headA);
+        int lenB=getListLength(headB);
+
+        while(lenA>lenB){
+            lenA--;
+            headA=headA.next;
+        }
+        while(lenB>lenA){
+            lenB--;
+            headB=headB.next;
+        }
+
+        while(headA!=headB){
+            headA=headA.next;
+            headB=headB.next;
+        }
+        return headA;
+        
+    }
+}
