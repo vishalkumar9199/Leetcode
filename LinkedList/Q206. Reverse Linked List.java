76782ac93b9e0cd21911e4613af4a74ed5bf37c6
vishal <128027462+vishalkumar9199@users.noreply.github.com>
2025-10-15 01:27:55 +0530

    Create Q206. Reverse Linked List.java

---
+++ LinkedList/Q206. Reverse Linked List.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+       if(head==null){
+        return null;
+       }
+       if(head.next==null){
+        return head;
+       }
+       ListNode before=null;
+       ListNode temp=head;
+
+       while(temp!=null){
+        ListNode after=temp.next;
+        temp.next=before;
+        before=temp;
+        temp=after;
+       }
+       head=before;
+       return head;
+    }
+}
