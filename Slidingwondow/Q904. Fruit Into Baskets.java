efd7fe94c2d8cb6bf10c06ca37a5f8ced2dfb2b0
vishal <128027462+vishalkumar9199@users.noreply.github.com>
2026-03-01 23:41:23 +0530

    Update Q904. Fruit Into Baskets.java

--- Slidingwondow/Q904. Fruit Into Baskets.java
+++ Slidingwondow/Q904. Fruit Into Baskets.java
@@ -1,25 +1,29 @@
-class Solution {
-    public int totalFruit(int[] fruits) {
-        Map<Integer,Integer>basket=new HashMap<>();
+import java.util.*;
+class Main {
+    public static int maxfruits(int[]fruits){
+        Map<Integer,Integer>map=new HashMap<>();
         int left=0;
         int right=0;
         int maxfruit=0;
-
-        for(right=0;right<fruits.length;right++){
-            int currCount=basket.getOrDefault(fruits[right],0);
-            basket.put(fruits[right],currCount+1);
-
-            while(basket.size()>2){
-                int fruitcount=basket.get(fruits[left]);
-                if(fruitcount==1)
-                   basket.remove(fruits[left]);
-                else
-                   basket.put(fruits[left],fruitcount-1); 
-                   left++;  
+        
+        for(right=0;right<nums.length;right++){
+            int currcount=map.getOrDefault(fruits[right],0);
+            map.put(fruits[right],currcount+1);
+            
+            while(map.size()>2){
+                int fruitcount=map.get(fruits[left]);
+                if(fruitcount==1){
+                    map.remove(fruits[left]);
+                }else
+                map.put(fruits[left],fruitcount-1);
+                left++;
             }
             maxfruit=Math.max(maxfruit,right-left+1);
         }
         return maxfruit;
-        
+    }
+    public static void main(String[] args) {
+        int[]fruits={1,2,3,2,2};
+        System.out.println(maxfruits(fruits));
     }
 }
