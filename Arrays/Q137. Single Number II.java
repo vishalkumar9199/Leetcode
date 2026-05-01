2ea1562a74b9dc36dfac5c0b85d98dc08ad13b9d
vishal <128027462+vishalkumar9199@users.noreply.github.com>
2026-05-01 22:49:09 +0530

    Create Q137. Single Number II.java

---
+++ Arrays/Q137. Single Number II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+         if(nums.length==1) return nums[0];
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        for(int num:nums){
+            if(map.get(num)==1){
+                return num;
+            }
+        }
+        return 0;
+        
+        
+    }
+}
