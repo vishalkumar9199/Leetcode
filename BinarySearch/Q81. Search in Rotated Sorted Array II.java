3ac495273f8967f3ec9818e0c7cec8c1c667c61a
vishal <128027462+vishalkumar9199@users.noreply.github.com>
2026-07-08 15:40:33 +0530

    Create Q81. Search in Rotated Sorted Array II.java

---
+++ BinarySearch/Q81. Search in Rotated Sorted Array II.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        Arrays.sort(nums);
+        int left=0;
+        int right=nums.length-1;
+
+        while(left<=right){
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]==target){
+                return true;
+            }else if(nums[mid]<target){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return false;
+    }
+}
