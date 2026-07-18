430581e6d375ca96bc83c2be6e5be586abe6b612
vishalkumar9199 <kr.vishal0123@gmail.com>
2026-07-18 14:57:30 +0530

    Fibonacci

---
+++ ArraysBasics/Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci{
+    public static int  fibonacci(int n){
+        if(n<=1){
+            return n;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+          System.out.print(fibonacci(i)+" ");
+        }
+
+        
+    }
+}
