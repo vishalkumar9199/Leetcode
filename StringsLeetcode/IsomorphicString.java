31ffaf5307776a1d3f88a1c97337a468a104f6ca
vishalkumar9199 <kr.vishal0123@gmail.com>
2026-08-09 14:41:04 +0530

    IsomorphicString

---
+++ StringsLeetcode/IsomorphicString.java
@@ -0,0 +1,38 @@
+package StringsLeetcode;
+
+import java.util.*;
+public class IsomorphicString{
+    public static boolean isomorphicString(String s,String t){
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character,Character>map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char original=s.charAt(i);
+            char replace=t.charAt(i);
+
+            if(!map.containsKey(original)){
+                if(!map.containsValue(replace)){
+                    map.put(original,replace);
+                }else{
+                    return false;
+                }
+            }else{
+                char ch=map.get(original);
+                if(ch!=replace){
+                    return false;
+                }
+            }
+           
+        }
+
+           return true;
+    }
+
+    public static void main(String[] args) {
+        String s="egg";
+        String t="add";
+
+        System.out.println(isomorphicString(s, t));
+    }
+}
