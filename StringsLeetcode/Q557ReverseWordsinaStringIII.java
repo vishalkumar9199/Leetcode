bdaff393e3eb730bf3661ddc8b002adcd69d7ebf
vishalkumar9199 <kr.vishal0123@gmail.com>
2026-08-27 00:17:27 +0530

    Reverse Word in a String 3

---
+++ StringsLeetcode/Q557ReverseWordsinaStringIII.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Q557ReverseWordsinaStringIII{
+    private static String Reverses(String s){
+        Stack<Character>stack=new Stack<>();
+
+        for(char ch:s.toCharArray()){
+            stack.push(ch);
+        }
+        StringBuilder sb= new StringBuilder();
+
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return sb.toString();
+    }
+    public static String ReverseWordInString(String s){
+        StringBuilder sb = new StringBuilder();
+        String[]str=s.split(" ");
+
+        for(String st:str){
+            sb.append(Reverses(st));
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+
+    }
+    public static void main(String[] args) {
+        String s="Vishal kumar";
+        System.out.println(ReverseWordInString(s));
+        
+    }
+}
